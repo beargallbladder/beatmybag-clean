b887fc50f71d96126681323e577f866f6c8a59d7
Sam Kim <samkim@samkim.com>
2025-06-01 22:06:15 -0700

    Add LoginPage to App - should see welcome screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
+import { LoginPage } from "./pages/LoginPage"
+
 function App() {
   console.log("App component rendering");
   
-  return (
-    <div style={{ padding: "50px", backgroundColor: "lightgreen" }}>
-      <h1>BeatMyBag App is Loading!</h1>
-      <p>If you see this, React is working.</p>
-      <button onClick={() => alert("Button clicked!")}>
-        Test Button
-      </button>
-    </div>
-  )
+  return <LoginPage />
 }
 
 export default App
